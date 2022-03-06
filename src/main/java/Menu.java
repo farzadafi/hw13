ab8f70013395e6be343a679ddce1d0a19c03a99a
farzad <farzadafi40@gmail.com>
2022-03-06 13:15:22 +0330

    add meinMenu method

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Menu {
+    private Scanner input = new Scanner(System.in);
+    private int command;
+
+    public void mainMenu(){
+        System.out.println("\n**********WELCOME**********");
+        System.out.println("1-Enter.");
+        System.out.println("2-Exit.");
+        System.out.print("Please enter your command:");
+        command = input.nextInt();
+        switch(command) {
+            case 1:
+                //
+                break;
+            case 2:
+                System.out.println("Have a nice day!");
+                System.exit(0);
+            default:
+                System.out.println("you enter a wrong number!");
+        }
+    }
+
 }
