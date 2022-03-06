23ec36d2eaf5ad5780356dc8d2ee4e48243305e7
farzad <farzadafi40@gmail.com>
2022-03-06 13:08:14 +0330

    create class SessionFactorySingleton for connection to DB

---
+++ src/main/java/repository/SessionFactorySingleton.java
@@ -0,0 +1,35 @@
+package repository;
+
+import entity.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactorySingleton {
+
+    private SessionFactorySingleton() {}
+
+    private static class LazyHolder {
+        static SessionFactory INSTANCE;
+
+        static {
+            var registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+
+            INSTANCE = new MetadataSources(registry)
+                    .addAnnotatedClass(UserAccount.class)
+                    .addAnnotatedClass(Student.class)
+                    .addAnnotatedClass(Professor.class)
+                    .addAnnotatedClass(OfferLesson.class)
+                    .addAnnotatedClass(Lesson.class)
+                    .addAnnotatedClass(Clerk.class)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+}
