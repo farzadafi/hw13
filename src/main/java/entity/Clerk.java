bb0586ef4c25b51a9608b269d01ab88594efc271
farzad <farzadafi40@gmail.com>
2022-03-06 12:53:51 +0330

    create clerk and add all filed

---
+++ src/main/java/entity/Clerk.java
@@ -0,0 +1,23 @@
+package entity;
+
+import lombok.*;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@DiscriminatorValue("Clerk")
+public class Clerk extends UserAccount {
+    private Integer salary;
+
+    public Clerk(Integer id, String fullName, String nationalId, String password) {
+        super(id, fullName, nationalId, password);
+        this.salary = 100000;
+    }
+
+}
