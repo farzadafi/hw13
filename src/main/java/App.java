3c05575c735bef548d2cbdf689ee7899c2c25ae5
farzad <farzadafi40@gmail.com>
2022-03-06 13:22:47 +0330

    add while loop for Rum all app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,24 @@
-import repository.SessionFactorySingleton;
-
 public class App {
     public static void main(String[] args) {
+        Menu menu = new Menu();
+        while (true) {
+            menu.mainMenu();
+            switch (menu.publicLogin()) {
+                case 0:
+                    break;
+
+                case 1:
+                    menu.clerkMenu();
+                    break;
+
+                case 2:
+                    menu.studentMenu();
+                    break;
+
+                case 3:
+                    menu.professorMenu();
+            }
+        }
     }
 }
+
