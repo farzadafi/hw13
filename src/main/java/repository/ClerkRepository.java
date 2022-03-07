04a1c578c713c4e36c0ae39b2f533a7461c64da9
farzad <farzadafi40@gmail.com>
2022-03-07 13:23:22 +0330

    fix findById method

--- src/main/java/repository/ClerkRepository.java
+++ src/main/java/repository/ClerkRepository.java
@@ -1,13 +1,25 @@
 package repository;
 
 import entity.Clerk;
+import entity.Professor;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class ClerkRepository implements Repository<Clerk> {
+
+    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+
     @Override
     public Clerk findById(int id) {
-        return null;
+        try (var session = sessionFactory.openSession()) {
+            Clerk clerk = session.find(Clerk.class,id);
+            if(clerk == null )
+                return null;
+            else
+                return clerk;
+        }
+
     }
 
     @Override
