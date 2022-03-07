1f2ffc118fa2f069374339009141009fc4d22f32
farzad <farzadafi40@gmail.com>
2022-03-07 20:36:08 +0330

    add showMyLesson method

--- src/main/java/repository/LessonRepository.java
+++ src/main/java/repository/LessonRepository.java
@@ -1,11 +1,18 @@
 package repository;
 
 import entity.Lesson;
+import entity.OfferLesson;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
 import service.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LessonRepository implements Repository<Lesson> {
+
+    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+
     @Override
     public Lesson findById(int id) {
         return null;
@@ -14,5 +21,18 @@ public class LessonRepository implements Repository<Lesson> {
     @Override
     public List<Lesson> findAll() {
         return null;
+    }
+
+    public List<Lesson> showMyLesson(int id){
+        try (var session = sessionFactory.openSession()) {
+            NativeQuery query = session.createSQLQuery("select * from lesson\n" +
+                                                          "join student_lessons sl on lesson.id = sl.lesson_id" +
+                                                           " where student_id = :id");
+            query.addEntity(Lesson.class);
+            query.setParameter("id",id);
+            List lessons = query.list();
+            return lessons;
+        }
+
     }
 }
