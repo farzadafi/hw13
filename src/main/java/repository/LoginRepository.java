1455416d18fb37f409ca0d2cfe3d94f89aff1e37
farzad <farzadafi40@gmail.com>
2022-03-07 07:56:41 +0330

    add FindByUserName mehtod

--- src/main/java/repository/LoginRepository.java
+++ src/main/java/repository/LoginRepository.java
@@ -1,4 +1,20 @@
 package repository;
 
+import entity.UserAccount;
+import org.hibernate.SessionFactory;
+
+
 public class LoginRepository {
+    private SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+
+    public UserAccount findByUserName(String nationalId){
+        try (var session = sessionFactory.openSession()) {
+            var query = session.createQuery("FROM Student as a WHERE a.nationalId = :nationalId",UserAccount.class);
+            query.setParameter("nationalId",nationalId);
+            var result =  query.getSingleResult();
+            return result;
+        }
+
+    }
+
 }
--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -1,4 +1,12 @@
 package service;
 
+import entity.UserAccount;
+import repository.LoginRepository;
+
 public class LoginService {
+    private final LoginRepository loginRepository = new LoginRepository();
+
+    public UserAccount FindByUserName(String userName){
+        return loginRepository.findByUserName(userName);
+    }
 }
