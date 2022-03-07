ebe2f59e0e3dab5bf08f7134fdacdbb1c95488b2
farzad <farzadafi40@gmail.com>
2022-03-07 13:11:46 +0330

    add findLesson method

--- src/main/java/repository/OfferLessonRepository.java
+++ src/main/java/repository/OfferLessonRepository.java
@@ -1,10 +1,14 @@
 package repository;
 
 import entity.OfferLesson;
+import entity.Professor;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class OfferLessonRepository implements Repository<OfferLesson> {
+    private SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+
     @Override
     public OfferLesson findById(int id) {
         return null;
@@ -13,5 +17,14 @@ public class OfferLessonRepository implements Repository<OfferLesson> {
     @Override
     public List<OfferLesson> findAll() {
         return null;
+    }
+
+    public OfferLesson findLesson(String lessonName) {
+        try (var session = sessionFactory.openSession()) {
+            var query = session.createQuery("FROM OfferLesson as a WHERE a.LessonName = :LessonName", OfferLesson.class);
+            query.setParameter("LessonName", lessonName);
+            var result = query.getSingleResult();
+            return result;
+        }
     }
 }
