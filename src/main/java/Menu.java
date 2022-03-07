e075749812aef18110b0b4c632b7c112eaeca40e
farzad <farzadafi40@gmail.com>
2022-03-07 08:22:38 +0330

    add clerkMenu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,9 +1,17 @@
+import entity.Student;
+import entity.UserAccount;
+import repository.GenericRepositoryImpel;
+import service.LoginService;
+import service.StudentService;
+
 import java.util.Scanner;
 
 public class Menu {
     private Scanner input = new Scanner(System.in);
     private int command;
     private String userName,password;
+    private final StudentService studentService = new StudentService();
+    private final LoginService loginService = new LoginService();
 
     public int mainMenu(){
         System.out.println("\n**********WELCOME**********");
@@ -33,9 +41,47 @@ public class Menu {
             System.out.println("farzad");
     }
 
-    public void clerkMenu(){
+    public void clerkMenu() {
+        boolean finalWhile = true;
+        while (finalWhile) {
+            System.out.println("\n****** Hi!******");
+            System.out.println("1-Student Tools.");
+            System.out.println("2-Professor Tools.");
+            System.out.println("3-Clerk Tools.");
+            System.out.println("4-Unit Tools.");
+            System.out.println("5-View Salary bill.");
+            System.out.println("6-Exit.");
+            System.out.print("Please select:");
+            command = input.nextInt();
+            input.nextLine();
+            switch (command) {
+                case 1:
+                    System.out.println("\n1-Register Student.");
+                    System.out.println("2-Delete Student.");
+                    System.out.println("3-Edit Student.");
+                    System.out.print("Please select a number:");
+                    command = input.nextInt();
+                    input.nextLine();
+                    switch (command) {
+                        case 1:
+                            studentService.add();
+                            break;
 
-    }
+                        case 2:
+                            break;
+
+                        case 3:
+                            break;
+
+                        default:
+                            System.out.println("you enter a wrong number!");
+                    }
+                    break;
+
+
+            }//switch
+        }//while
+    }//clerkMenu
 
     public void studentMenu(){
 
